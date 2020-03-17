fd056ddc6685d5b8dd394ef6f3453a5c95c2fe57
legezaanna7@gmail.com <legezaanna7@gmail.com>
2020-03-17 16:00:36 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,51 @@
-const fetchPromise = fetch('https://api.chucknorris.io/jokes/random');
-fetchPromise.then(response => {
-  return response.json();
-  
-}).then(random => {
-  console.log(random)
-    const jokeContainer = document.getElementsByClassName('jokes')[0];
-    const p = document.createElement('p');
-    const image = document.createElement('img');
-    p.innerText = random.value;
-    image.src = random.icon_url;
-    jokeContainer.appendChild(p);
-    jokeContainer.appendChild(image);
+function getJoke() {
+  const jokeContainer = document.getElementsByClassName("jokes")[0];
+  const p = document.createElement("p");
+  const image = document.createElement("img");
+  const fetchPromise = fetchChuck();
+  const hiden = document.body;
+  fetchPromise
+    .then(response => {
+      // console.log(response);
+      return response.json();
+      
+    })
+    .then(random => {
+      // console.log(random);
+      p.innerText = random.value;
+      image.src = random.icon_url;
+  jokeContainer.innerHTML = '';
+
+      jokeContainer.appendChild(p);
+      jokeContainer.appendChild(image);
+
+      
+      
+    });
+
     
+    
+    
+    
+}
+
+
+function initBtnFunctionality() {
+  const btn = document.querySelector("button");
+  btn.addEventListener("click", function() {
+    console.log("Hello");
+    getJoke();
    
     
-});
+  });
+}
+
+initBtnFunctionality();
+getJoke();
 
 
-const btn = document.querySelector('button');
-console.log(btn);
- 
+function fetchChuck() {
+  return fetch("https://api.chucknorris.io/jokes/random");
 
-btn.addEventListener('click', function() {
-console.log('hello');
+}
 
-
-
-  
-
-
-
-});
